2182bd311d7b110d6dfa9bc1291f2de8cc912930
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2023-06-20 03:13:29 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
-  root to: 'pages#home'
+  devise_for :admins, controllers: {
+    sessions: "admin/sessions"
+  }
+
+  devise_for :customers, controllers: {
+    sessions: "customer/sessions",
+    registrations: "customer/registrations"
+  }
+
+  root to: "pages#home"
 
   get '/up/', to: 'up#index', as: :up
   get '/up/databases', to: 'up#databases', as: :up_databases
